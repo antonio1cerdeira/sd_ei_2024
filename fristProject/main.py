3ccee7543ca762f14e9948a11b007bb1855f5d34
AntonioCerdeira <antonio.cerdeira@gmail.com>
2024-03-04 16:19:11 +0000

    Update FistProgram

---
+++ fristProject/main.py
@@ -0,0 +1,50 @@
+"""Comentarios multilinha"""
+# comntarios de linha
+# atribuição de variaveis primeiro programa em python
+print("ola mundo!")
+# atribuir um valor a x
+x=3
+# atribuir um valor a y
+y = 5
+y = "John"
+print("x= ",x)
+print("y= ",y)
+
+# identifica o tipo de dados
+x = 5
+y = "John"
+# type indica o tipo da variavel introduzidas
+print(type(x))
+print(type(y))
+#
+x = 5
+y = "John"
+Y = "case-sensitve"
+# type indica o tipo da variavel
+print(type(x))
+print(type(y))
+print(x,y,Y)
+# cast, definir tipos de dados intoduzidos
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+print(x,y,x)
+print(type(x),type(y),type(x))
+# operaçoes basicas de somar
+x = "Python is "
+y = "awesome"
+z =  x + y
+x = 5
+y = 10
+print(z,",",x+y)
+# esturturas de controlo por condição ou repetição
+x = 5
+y = 2
+if x > y:
+ print("x é maior que y")
+else:
+ print("x não é maior que y")
+i = 1
+while i < 6:
+ print(i)
+ i += 1
