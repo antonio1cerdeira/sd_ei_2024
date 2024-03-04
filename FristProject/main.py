56e6e24b2c717af25a71aca552ae21b9bf68533a
AntonioCerdeira <antonio.cerdeira@gmail.com>
2024-03-04 15:52:21 +0000

    UpdateFristProgram

---
+++ FristProject/main.py
@@ -0,0 +1,49 @@
+"""# firsts python programs
+print("ola mundo!")
+x=3
+print("x= ",x)
+(comentarios multlinha)"""
+
+# firsts python programs
+print("ola mundo!")
+# atribuir um valor a x
+x=3
+print("x= ",x)
+
+# como mostrar os valor atribuidos as vaiaveis
+print("ola mundo!")
+x=3
+print("x= ",x)
+
+# descreve o tipo de dados da variavel
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+# cast, definir tipos que se pretendem
+x = str(3) # x will be '3' string
+y = int(3) # y will be 3
+z = float(3) # z will be 3.0
+print(x,y,x)
+print(type(x),type(y),type(x))
+#operações basicas
+x = "Python is "
+y = "awesome"
+z = x + y
+x = 5
+y = 10
+print(z,",",x+y) #porque a string aparece primeiro
+#importancia da identação,estruturas de controlo de decição e de repetição (ifs,ciclos)
+#defenimos as variaveis executa as condiçoes apresenta mensagem excuta o ciclo de contagem
+x = 5
+y = 2
+if x > y:
+    print("x é maior que y")
+else:
+    print("x não é maior que y")
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+
+
