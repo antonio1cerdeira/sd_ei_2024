02f777a1c8303e5b3d29638f510d4b1bea41cce5
AntonioCerdeira <antonio.cerdeira@gmail.com>
2024-03-20 11:25:11 +0000

    Ficha5_1-7

--- rpc/SimpleXMLRPCServer.py
+++ rpc/SimpleXMLRPCServer.py
@@ -12,13 +12,52 @@ with SimpleXMLRPCServer(('localhost', 8000),
 
     # Register pow() function; this will use the value of
     # pow.__name__ as the name, which is just 'pow'.
-    server.register_function(pow)
+    #server.register_function(pow)
 
     # Register a function under a different name
     def adder_function(x, y):
         return x + y
     server.register_function(adder_function, 'add')
+    def sub_function(x, y):
+        return x - y
+    server.register_function(sub_function, 'sub')
+    def div_function(x, y):
+        return x / y
+    server.register_function(div_function, 'div')
+    def somatorio_function(z):
+        total = 0
+        for i in range (1, z+1):
+            total += z**3
+        return total
+    server.register_function(somatorio_function, 'Soma')
+    def imaginarios_function(a1, i1, a2, i2):
+        parteReal=a1+a2
+        parteIm=i1+i2
+        return(parteReal, parteIm)
+    server.register_function(somatorio_function, 'imaginarios_function')
 
+    def mulimaginarios_function(a1, i1, a2, i2):
+        parte_real = (a1 * a2) - (i1 * i2)
+        parte_imaginaria = (a2 * i1) + (a1 * i2)
+        return parte_real, parte_imaginaria
+    server.register_function(mulimaginarios_function, 'mulimaginarios_function')
+    def Calq_Primo(p):
+        if p <= 1:
+            return False
+        elif p <= 3:
+            return True
+        elif p % 2 == 0 or p % 3 == 0:
+            return False
+        i = 5
+        while i * i <= p:
+            if p % i == 0 or p % (i + 2) == 0:
+                return False
+            i += 6
+        return True
+    server.register_function(Calq_Primo, 'Calq_Primo')
+    def allPrime(x):
+        primes = [i for i in range(2, x + 1) if Calq_Primo(i)]
+        return primes
     # Register an instance; all the methods of the instance are
     # published as XML-RPC methods (in this case, just 'mul').
     class MyFuncs:
--- rpc/client.py
+++ rpc/client.py
@@ -3,10 +3,30 @@ import xmlrpc.client
 s = xmlrpc.client.ServerProxy('http://localhost:8000')
 x= int(input("entre o 1-valor: "))
 y=int(input("entre o 2-valor: "))
-print(type(x))
-print(s.pow(x%150,y%10))  # Returns 2**3 = 8
-print(s.add(x,y))  # Returns 5
-print(s.mul(x,y))  # Returns 5*2 = 10
+print("---------Somatorio----------")
+z=int(input("entre o valor para o somatorio: "))
+print("---------numero imaginario 1 ----------")
+a1=float(input("parte real "))
+i1=float(input("parte imagiaria"))
+print("---------numero imaginario 2 ----------")
+a2=float(input("parte real "))
+i2=float(input("parte imagiaria"))
+print("---------numero primo ----------")
+p=int(input("entre o valor para o numero primo:"))
 
-# Print list of available methods
+print(type(x))
+print(s.add(x,y))  # Returns 2+3 = 5
+print(s.sub(x,y))  # Returns 5-2=3
+print(s.div(x,y))  # Returns 6/2 = 3
+print(s.mul(x,y))  # Returns 5*2 = 10
+print("---------sumatorio ----------")
+print(s.Soma(z))
+print("---------soma dos imaginarios ----------")
+parteReal, parteIm = s.imaginarios_function(a1, i1, a2, i2)
+print("complexo é {} + {}i".format(parteReal, parteIm))
+print("---------Multiplicação dos imaginarios ----------")
+parteReal, parteIm = s.mulimaginarios_function(a1, i1, a2, i2)
+print("mul do complexo é {} + {}i".format(parteReal, parteIm))
+print("---------numeros primos ----------")
+print(s.Calq_Primo(p))
 print(s.system.listMethods())
